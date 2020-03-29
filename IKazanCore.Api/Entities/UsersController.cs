955192477283cb9cf903375f63dec7ff03231e72
Kazan <vtdthangkhtn@gmail.com>
2020-03-29 15:57:27 +0700

    First Commit

---
+++ IKazanCore.Api/Entities/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKazanCore.Api.Entities
+{
+    [Route("api/v1/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            var abc = new
+            {
+                id = 1,
+                name = "Test 1"
+            };
+
+            return StatusCode(StatusCodes.Status200OK, abc);
+        }
+    }
+}
